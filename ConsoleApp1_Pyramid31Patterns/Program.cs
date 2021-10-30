c0d383dee44ce52342ec612a3a407db85c09745f
Samikshya <write2samikshyakar@gmail.com>
2021-10-30 13:43:55 +0530

    Add project files.

---
+++ ConsoleApp1_Pyramid31Patterns/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleApp1_Pyramid31Patterns.Pyramid31Pattterns;
+
+namespace ConsoleApp1_Pyramid31Patterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Pyramid1.MyPyramid1Display();
+        }
+    }
+}
---
+++ ConsoleApp1_Pyramid31Patterns/Pyramid31Pattterns/Pyramid1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1_Pyramid31Patterns.Pyramid31Pattterns
+{
+   public  class Pyramid1
+    {
+        public static void MyPyramid1Display()
+        {
+            Console.WriteLine(" Enter the Number of the Rows ");
+            int rows = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<=rows; i++)
+            {
+                for (int j = rows; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+        }
+        
+    }
+}
